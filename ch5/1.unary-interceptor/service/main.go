30290f400d54aff65c229934754be3c7b2109deb
Petru <smart__petea@mail.ru>
2020-08-15 12:22:20 +0300

    unary interceptor

--- ch5/1.unary-interceptor/service/main.go
+++ ch5/1.unary-interceptor/service/main.go
@@ -20,13 +20,33 @@ const (
     port = ":50051"
 )
 
+func orderUnaryServerInterceptor(ctx context.Context,
+    req interface{},
+    info *grpc.UnaryServerInfo,
+    handler grpc.UnaryHandler,
+) (interface{}, error) {
+    //Preprocessing logic
+    //Gets info about the current RPC call by examining the args passed in
+    log.Println("======[Server Interceptor] ", info.FullMethod) //1. Preprocessing phase: this is where you can intercept the message prior to invoking the respective RPC
+
+    //Invoking the handler to complete the normal execution of a unary RPC
+    m, err := handler(ctx, req) //2. Invoking the RPC method via UnaryHandler
+
+    //Post processing logic
+    log.Printf(" Post Proc Message : %s", m) //3. Postprocessing phase: you can process the response from the RPC invocation
+
+    return m, err //4. Sending back the RPC response
+}
+
 func main() {
     lis, err := net.Listen("tcp", port)
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
     }
 
-    s := grpc.NewServer()
+    s := grpc.NewServer(
+        grpc.UnaryInterceptor(orderUnaryServerInterceptor), //5. Registering the unary interceptor with the gRPC server
+    )
     h.RegisterHelloManagementServer(s, &Server{})
 
     log.Printf("Starting gRPC listener on port " + port)
