254ae4e6854404eccfa792e7b100b8c20e81c331
Petru <smart__petea@mail.ru>
2020-08-22 14:18:35 +0300

    grpcurl

--- studies/1.api/grpc/main.go
+++ studies/1.api/grpc/main.go
@@ -1,7 +1,37 @@
 package main
 
 import (
+    "net"
+    "log"
+    "google.golang.org/grpc"
+	empty "github.com/golang/protobuf/ptypes/empty"
+	wrappers "github.com/golang/protobuf/ptypes/wrappers"
+    "context"
+
+    s "grpc/proto"
 )
 
+const (
+    port = ":10000"
+)
+
+type Server struct {}
+
+func (server *Server) HomePage(ctx context.Context, empt *empty.Empty) (*wrappers.StringValue, error) {
+    return &wrappers.StringValue{Value: "Welcome to the HomePage!"}, nil
+}
+
 func main() {
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+
+    gServer := grpc.NewServer()
+    s.RegisterServiceAServer(gServer, &Server{}) 
+
+    log.Printf("Starting gRPC listener on port " + port)
+    if err := gServer.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
 }
---
+++ studies/1.api/grpc/test.sh
@@ -0,0 +1,1 @@
+grpcurl --import-path proto --proto u.proto -plaintext localhost:10000 proto.ServiceA/homePage
